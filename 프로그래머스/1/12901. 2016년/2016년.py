a60636e368daa011e2511191b881c5f0ed899c9f
kiri5358 <111713822+kiri5358@users.noreply.github.com>
2026-07-08 13:48:39 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -0,0 +1,10 @@
+def solution(a, b):
+    answer = '' 
+
+    months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    days_of_week = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"]
+    total_days = sum(months[:a-1]) + b
+    
+    answer = days_of_week[total_days % 7]
+    
+    return answer
